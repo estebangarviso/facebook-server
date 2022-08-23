12a10c1cd6a915addca6adeae64efb1db3b3d100
Esteban Garviso <e.garvisovenegas@gmail.com>
2022-08-23 12:04:11 -0400

    adding Access-Control-Allow-Credentials true

--- src/middlewares/beforeAuthenticate.ts
+++ src/middlewares/beforeAuthenticate.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express";
 
 const beforeAuthenticate: RequestHandler = (req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  res.append("Access-Control-Allow-Credentials", "true");
+  next();
 };
 export default beforeAuthenticate;
